15a3f29f0a9ac3c3812ec75418f3663934c54f56
Jason <es6513@gmail.com>
2020-11-07 19:25:36 +0800

    extract form scss

--- src/context/taskLists/mockData.js
+++ src/context/taskLists/mockData.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 
 const mockInitialState = {
-  taskLists: new Array(10).fill(),
+  taskLists: new Array(5).fill(),
   timer: {
     currentId: "",
     isCountDown: false,
@@ -11,9 +11,9 @@ const mockInitialState = {
 mockInitialState.taskLists = mockInitialState.taskLists.map((el, index) => {
   const task = {
     id: uuidv4(),
-    isArchived: Math.random() > 0.5 ? true : false,
-    isDone: Math.random() > 0.5 ? true : false,
-    isBreak: Math.random() > 0.5 ? true : false,
+    isArchived: false,
+    isDone: false,
+    isBreak: false,
     taskTitle: String(index + 1),
     estimatedTomato: index + 1,
   };
